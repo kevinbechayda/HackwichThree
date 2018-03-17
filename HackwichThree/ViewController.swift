26bb60b50ad965c381b55234e27bd8e92861aa59
Kevin Bechayda <kevinbechayda@gmail.com>
2018-03-17 13:18:09 -1000

    Finished Tab 1

--- HackwichThree/ViewController.swift
+++ HackwichThree/ViewController.swift
@@ -10,15 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet var firstLabel: UILabel!
+    @IBOutlet var secondLabel: UILabel!
+    @IBOutlet var thirdLabel: UILabel!
+    @IBOutlet var fourthLabel: UILabel!
+    @IBOutlet var fifthLabel: UILabel!
     
+    @IBOutlet var classesTextView: UITextView!
+    
+    @IBOutlet var profilePic: UIImageView!
+    @IBOutlet var logo: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
 //set firstLabel to the string name "about"
-self.firstLabel.text = "About"
+        self.firstLabel.text = "My Name is:"
+        self.secondLabel.text = "Kevin B."
+        self.thirdLabel.text = "My Major is:"
+        self.fourthLabel.text = "Creative Media"
+        self.fifthLabel.text = "My Spring 2018 classes are:"
+
+        self.classesTextView.text = "CM 150, CM 161, CM 400, CM 486"
+        
+        profilePic.image = UIImage(named:"ProfilePic.jpg")
+        logo.image = UIImage(named:"ACM Logo.jpg")
+
         
     }
 
